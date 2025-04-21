2720708fbe1860d2c464ceca05a4de66e19d67e4
gabriel1003 <gabriel.samersla97@gmail.com>
2025-04-21 10:55:20 -0300

    feat: add class to generate uppercase letter

---
+++ lib/generate_password/generate_letter_upercase.dart
@@ -0,0 +1,18 @@
+import 'generate_random_number.dart';
+
+class GenerateLetterUppercase {
+  String letterUppercase() {
+    final uppercaseLetters = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+    final randomNumberGenerator = GenerateRandomNumber();
+    final index = randomNumberGenerator.generate();
+
+    if (index >= 0 && index < uppercaseLetters.length) {
+        return uppercaseLetters[index];
+    } else {
+        return '';
+    }
+  }
+}
